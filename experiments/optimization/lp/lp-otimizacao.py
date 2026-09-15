b41c4751d8671ddc8bab2eeebdd030b409741b8d
Emilio <chem.montenegro.eq@gmail.com>
2026-09-15 09:34:52 -0500

    chore: lp of otimizacao and scipy updated

--- experiments/optimization/lp/lp-otimizacao.py
+++ experiments/optimization/lp/lp-otimizacao.py
@@ -4,4 +4,4 @@ otimizar.adicionar_funcao_objetivo('min. + 3000x_1 + 20000x_2 + 30000x_3 + 10000
 otimizar.adicionar_restricao('x_1 + x_2 + x_3 + x_4 == 20')
 otimizar.adicionar_restricao('+ 20x_1 + 5x_2 + 10x_3 + 2x_4 <= 200')
 otimizar.adicionar_restricao('+ 10x_1 + 20x_2 + 20x_3 + 15x_4 >= 80')
-otimizar.simplex(calculo_visivel=False)
+otimizar.simplex(calculo_visivel=True)
--- experiments/optimization/lp/lp-scipy.py
+++ experiments/optimization/lp/lp-scipy.py
@@ -1,12 +1,11 @@
 from scipy.optimize import linprog
-
 A_ub = [[ 20,     5,   10,   2],
         [-10,   -20,  -20, -14] ]
 b_ub = [ 200 , -80 ]
 A_eq = [ [1, 1, 1, 1] ]
 b_eq = [20]
 c = [ 3000, 20000, 30000, 10000 ]  
-
-linprog(c, A_ub=A_ub, A_eq=A_eq, 
+results = linprog(c, A_ub=A_ub, A_eq=A_eq, 
         b_eq=b_eq, b_ub=b_ub, 
         method="Highs")
+print(results)
